553aa7d36e1b47525ed534b6f24587179e5a3b95
Christopher Hahn <christopher.hahn@artcom.de>
2017-10-13 00:06:14 +0200

    [add] Random Spawn

--- Assets/Scripts/WaveSystem/Wave.cs
+++ Assets/Scripts/WaveSystem/Wave.cs
@@ -9,15 +9,28 @@ public enum WaveType{
 	/// </summary>
 	Normal = 15,
 	Fast = 10,
-	Group = 2
+	Group = 2,
+	BigGroup = 1
+}
+
+public enum SpawnType{
+	One,
+	Random,
+	AllSymetric,
+	Row
 }
 
 [System.Serializable]
 public class Wave{
 
 	public WaveType waveType;
+
 	[Header("Number of Different Enemies in Wave")]
 	public List<Enemy> EnemiesToSpawn = new List<Enemy>();
+
+	public SpawnType spawnType = SpawnType.One;
+	[SerializeField]
+	public bool[] SpawnActive = new bool[4];  
 
 	float time;
 	float SpawnSpeed;
@@ -49,13 +62,27 @@ public class Wave{
 			 
 			enemy.transform.localScale =  Vector3.one * ((int)EnemiesToSpawn [spawnCounter].size / 10f);
 			enemy.GetComponent<StartSearch> ().target = manager.target;
-			enemy.transform.position = manager.Spawns [spawnCounter].transform.position; //TODO: Spawn on diffrent locations
+			enemy.transform.position = SelectSpawn(manager);// manager.Spawns [spawnCounter].transform.position; //TODO: Spawn on diffrent locations
 			enemy.GetComponent<EnemyBehavior> ().MetalCollector = manager.MetalCollector;
 			enemy.transform.SetParent (manager.EnemyCollector.transform);
 		} else {
 			amountCounter = 0;
 			spawnCounter++;
 		}
+	}
+
+	Vector3 SelectSpawn(WaveManager manager){
+		GameObject Spawn;
+		switch (spawnType) {
+			case SpawnType.Random:
+			Spawn = manager.Spawns [Random.Range(0,manager.Spawns.Length - 1)];
+			break;
+
+			default: 
+				Spawn = manager.Spawns [0];
+				break;
+		}
+		return Spawn.transform.position;
 	}
 }
 
