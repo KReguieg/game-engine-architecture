f3f8deada0654c712a0a08ba58898a84703c265a
Christopher Hahn <christopher.hahn@artcom.de>
2017-10-13 23:05:19 +0200

    changed dummy movement to teleprotation

--- Assets/Scripts/StartSearch.cs
+++ Assets/Scripts/StartSearch.cs
@@ -6,25 +6,9 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class StartSearch : MonoBehaviour {
 	public GameObject target;
-	public LayerMask mask;
-	public bool Controlleble = false;
 	// Use this for initialization
 	void Start () {
 		GetComponent<NavMeshAgent> ().SetDestination(target.transform.position);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Controlleble && Input.GetMouseButtonDown(0)) {
-			
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			RaycastHit hit;
-
-			if (Physics.Raycast (ray, out hit, 1000, mask)) {
-				if(hit.collider.gameObject.tag != "player")
-					GetComponent<NavMeshAgent> ().SetDestination(hit.point);
-			}
-		}
 	}
 
 
