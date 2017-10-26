faae9d9e6350f253d23aaad1d79c9fa4ab736a62
Christopher Hahn <christopher.hahn@artcom.de>
2017-10-26 15:00:11 +0200

    [ADD] added Door open Animation

---
+++ Assets/Scripts/DoorOpener.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpener : MonoBehaviour {
+	Animation animation;
+	// Use this for initialization
+	void Start () {
+		animation = GetComponent<Animation> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void StartOpeningDoor(){
+		animation.Play ();
+	}
+}
