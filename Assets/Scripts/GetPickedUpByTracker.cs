614a45cdd20895023af062cf161b6fd7f44fa13a
Khaled <khaled.reguieg@artcom.de>
2017-11-09 16:46:22 +0100

    [ADD] new VR Rifle and sword to play with.

---
+++ Assets/Scripts/GetPickedUpByTracker.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetPickedUpByTracker : MonoBehaviour {
+
+		ConstantForce force;
+
+		Rigidbody boody;
+
+		GameObject tracker;
+
+	void OnTriggerEnter(Collider col) {
+		Debug.Log(col.gameObject.name);
+		if(col.name == "Kran") {
+			tracker = col.gameObject;
+			tracker.GetComponent<Rigidbody>().isKinematic = true;
+			tracker.AddComponent<SpringJoint>();
+			
+			SpringJoint springy = tracker.GetComponent<SpringJoint>();
+			springy.breakForce = 100.0f;
+			tracker.GetComponent<SpringJoint>().connectedBody = boody;
+			tracker.GetComponent<SpringJoint>().spring = 25;
+			tracker.GetComponent<SpringJoint>().anchor *= -1;
+			transform.parent = col.transform;
+			boody.drag = 35;
+			transform.localPosition += new Vector3(0, -0.01f, 0);
+		}
+		if (col.name == "TargetboxFloor") {
+			Debug.Log("TARGETBOX, DROP THAT SHIT!!!");
+			Destroy(tracker.GetComponent<SpringJoint>());
+			transform.parent = null;
+			GameObject.Find("Boxes").GetComponent<Animator>().enabled = true;
+		}
+		//col.transform.parent.GetComponent<BoxCollider>().isTrigger = false;
+	}
+
+	void OnCollision(Collision collision) {
+		Debug.Log(collision.gameObject.name);
+		if(collision.collider.name.Contains("Treasurebox") || collision.collider.name.Contains("Sphere") ) {
+			boody.AddForce(new Vector3(0, -0.1f, 0));
+		}
+	}
+
+	void Start() {
+		boody = GetComponent<Rigidbody>();
+		force = GetComponent<ConstantForce>();
+	}
+}
--- Assets/VRTK/Examples/ExampleResources/Scripts/Gun.cs
+++ Assets/VRTK/Examples/ExampleResources/Scripts/Gun.cs
@@ -4,9 +4,20 @@
 
     public class Gun : VRTK_InteractableObject
     {
+        [SerializeField]
         private GameObject bullet;
+
+        [SerializeField]
+        private GameObject muzzlePoint;
         private float bulletSpeed = 1000f;
         private float bulletLife = 5f;
+
+        [SerializeField]
+        private AudioClip shotsFiredClip;
+
+        private AudioSource audio;
+
+        private LineRenderer lineRenderer;
 
         public override void StartUsing(VRTK_InteractUse usingObject)
         {
@@ -16,16 +27,29 @@
 
         protected void Start()
         {
-            bullet = transform.Find("Bullet").gameObject;
+            bullet = transform.Find("C_Bullet").gameObject;
             bullet.SetActive(false);
+            audio = GetComponent<AudioSource>();
+            audio.clip = shotsFiredClip;
+            lineRenderer = GetComponent<LineRenderer>();
         }
 
         private void FireBullet()
         {
             GameObject bulletClone = Instantiate(bullet, bullet.transform.position, bullet.transform.rotation) as GameObject;
+            Ray r = new Ray(muzzlePoint.transform.position, muzzlePoint.transform.forward);
+            RaycastHit hit;
+            if(Physics.Raycast(r, out hit))
+            {
+                lineRenderer.SetPosition(0, muzzlePoint.transform.position);
+                lineRenderer.SetPosition(1, hit.transform.position);
+            }
+            
             bulletClone.SetActive(true);
             Rigidbody rb = bulletClone.GetComponent<Rigidbody>();
-            rb.AddForce(-bullet.transform.forward * bulletSpeed);
+            rb.AddForce(bullet.transform.forward * bulletSpeed);
+
+            audio.Play();
             Destroy(bulletClone, bulletLife);
         }
     }
