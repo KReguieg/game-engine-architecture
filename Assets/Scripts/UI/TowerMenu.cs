6423287aaf0ac9cab2e43aa90737149bf5d28fa4
Christopher Hahn <derhahn91@web.de>
2017-11-11 12:15:14 +0100

    [FIX] TowerMenu link to RTS Camera

--- Assets/Scripts/UI/TowerMenu.cs
+++ Assets/Scripts/UI/TowerMenu.cs
@@ -18,6 +18,7 @@ public class TowerMenu : MonoBehaviour {
 	void Start () {
 		iUIManager = transform.parent.GetComponent<IntergratedUiManager> ();
 		rangeRotator = transform.parent.parent.GetComponentInChildren<RangeRotator>();
+		GetComponentInChildren<Canvas>().worldCamera = RTS_Camera.Camera;
 
 	}
 
