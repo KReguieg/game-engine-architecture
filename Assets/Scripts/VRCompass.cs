86164e0dd5dd599566064d2a7056a4fc0caef210
Christopher Hahn <derhahn91@web.de>
2017-12-27 19:35:00 +0100

    Added Compass to left hand of VR player

---
+++ Assets/Scripts/VRCompass.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRCompass : MonoBehaviour {
+	float angle;
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 direction = transform.parent.forward;
+		direction.y = 0;
+		angle = Vector3.Angle (direction, Vector3.forward);
+		if (direction.x > 0)
+		 	angle *= -1;
+		transform.localRotation = Quaternion.AngleAxis(angle, Vector3.up);
+	}
+
+	void OnDrawGizmos()
+	{
+		Vector3 start = transform.position;
+		Vector3 end = start += transform.parent.forward;
+		Gizmos.DrawLine(start, end);
+	}
+}
