d25d721bfc0549af2a8244fb0693591a94bae7d8
DerCodeHahn <derhahn91@web.de>
2018-01-19 11:22:18 +0100

    weapon unlock

---
+++ Assets/UnlockWeapon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnlockWeapon : MonoBehaviour {
+    public GameObject Weapon;
+    private void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(() => { UnlockWeaponUseable(); });
+    }
+
+    private void UnlockWeaponUseable()
+    {
+        Weapon.GetComponent<VRTK.VRTK_InteractableObject>().isUsable = true;
+    }
+}
