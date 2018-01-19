9f217dc6927ee4435263e4da226b211ff73d77b5
DerCodeHahn <derhahn91@web.de>
2018-01-19 10:46:28 +0100

    pickup weapon

---
+++ Assets/HolsterWeapon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolsterWeapon : MonoBehaviour {
+    public GameObject Holster;
+    public GameObject[] WeaponPrefabs;
+
+    public void HolsterWeaponNumber(int number)
+    {
+        if(Holster.transform.childCount != 0)
+            Destroy(Holster.transform.GetChild(0).gameObject);
+        GameObject equipedWeapon = Instantiate(WeaponPrefabs[number], Holster.transform);
+        equipedWeapon.transform.localRotation = Quaternion.identity;
+        equipedWeapon.transform.localPosition = Vector3.zero;
+    }
+}
